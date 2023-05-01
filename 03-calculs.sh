b32ee2334a7407cd9762e0ab4340e3b04f44f0c3
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 10:03:54 -0400

    Modification calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo 10+10
+echo $((10+10)) # Pour effectuer un calcul, il faut $(()) sinon echo va seulement afficher ce qui est indiquer dans l'echo
+echo $valeur
+
+read -p "nb 1 : " nb1
+read -p "nb 2 : " nb2
+let resultat=$nb1*$nb2
+echo $resultat
+# Deuxième façon de faire un calcul. Let indique que l'on fait un calcul
