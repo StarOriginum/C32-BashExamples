7150b155ead215c7bd7d47ce9f1b2ba054ea9243
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 11:05:33 -0400

    Ajout fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# -e = fichier existe
+# -f = c'est un fichier?
+# -d = c'est un dossier?
+if [[ -e $1 ]] # Il est possible d'utiliser if test et if [[]] de manière interchangée.
+then
+    echo "le fichier existe"
+fi
