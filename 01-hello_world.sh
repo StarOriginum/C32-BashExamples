d402a9c4488aba7e55d8d6a785ca32865f727cfb
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 09:46:39 -0400

    Modification Hello World

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo "Hello" $USER
+
+# exit
+echo -n "Vive "
+echo "Linux"
+
+# -e permet d'interpreter les retours de lignes et etc (\n, \t, ...)
+echo -e "1\n2\n3"
+# -p permet de spécifier un message
+# Quand on met quelque chose dans une variable, on utilise pas de $. Pour lire la valeur, on utilise $
+read -p "Quel age avez-vous? " age
+echo $age "! Vous etes jeune"
