c1cc2dd3b643d2d7e08706c5dc60a6d6cf07f026
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 10:43:49 -0400

    Modifications boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do 
+    echo $i
+done
+
+# '' = execution d'une commande que l'on affecte à une variable donnée. 
+liste=`ls`
+for fichier in $liste
+do 
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
