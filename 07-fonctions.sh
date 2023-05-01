f94b18048dfe82163bd90507d367ca2e84f5769a
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 10:56:04 -0400

    Ajout fonctions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+maFonction() {
+    echo "param : " $1 # $0 veut dire le nom de la fonction ou script, $1 veut dire le nom du paramètres, ainsi que suite.
+}
+
+maFonction "Salut" # Il n'y a pas de paranthèses lors de l'utilisation et l'execution de la fonction
+maFonction "toi"
