34d1d7c8b93d395c6dbfc8cbdbc895c9c27bff3f
Michael Turner <e.mturner@etu.cvm.qc.ca>
2023-05-01 09:56:51 -0400

    Modification Variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+# "" pour les mots composés
+prenom=Neo
+echo $prenom
+
+declare -i age=33
+declare -r CHEMIN=/bin
+
+echo "Nom script" $0
+echo "Nombre params" $#
+echo "Perso : " $1
